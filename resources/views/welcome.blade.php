ae001224bc391b4f4fc9b0cc99d1e3a620b63ac9
Kwok <guojam@outlook.com>
2018-03-05 07:55:56 +0000

    mine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Weibo-Kwok power by Laravel!
                 </div>
 
                 <div class="links">
