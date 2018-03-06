d17495bbe7004504fc664948c12e29722c7d5cf0
Kwok <guojam@outlook.com>
2018-03-06 06:17:11 +0000

    User database

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +11,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
+     *  为了提高应用的安全性，Laravel 在用户模型中默认为我们添加了 fillable 在过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新：
      * @var array
      */
     protected $fillable = [
@@ -20,10 +20,12 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     *  最后，当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏，则可使用 hidden 属性：
      * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $table = 'users';
 }
